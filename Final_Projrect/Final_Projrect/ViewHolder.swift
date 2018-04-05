793f33b768d1ba6cd428cf8680a8eb6d1c414ffe
stevewreese <stephenw.reese@gmail.com>
2018-04-05 17:41:52 -0600

    init

---
+++ Final_Projrect/Final_Projrect/ViewHolder.swift
@@ -0,0 +1,22 @@
+//
+//  ViewHolder.swift
+//  Final_Projrect
+//  holder to hold all the views
+//  Created by Stephen Reese on 4/5/18.
+//  Copyright © 2018 Stephen Reese. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewHolder: UIView
+{
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
