c93bb4539a4e12124d6fdfdcc9e30ae23f8ba2fc
stevewreese <stephenw.reese@gmail.com>
2018-04-23 15:44:44 -0600

    end of part1

--- openGLtest/openGLtest/ViewController.swift
+++ openGLtest/openGLtest/ViewController.swift
@@ -17,12 +17,34 @@ class ViewController: GLKViewController {
         glkView.context = EAGLContext(api: .openGLES2)!
         EAGLContext.setCurrent(glkView.context)
         
+        let vertexShaderSource: String = """
+        "
+            void main(){
+                gl_Position = vec4(1.0, 1.0, 0.0, 1.0);
+            }
+        "
+        """
+        
+        glColor4f(1.0, 0.0, 0.0, 1.0)
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        //TODO DRAW A TRIANGLE
+        let triangleData: [Float] = [
+            +0.80, -0.30,
+            -0.30, +0.70,
+            -0.65, -0.65,
+        ]
+        
+        glVertexAttribPointer(0, 2, GLenum(GL_FLOAT), GLboolean(GL_FALSE), 0, triangleData)
+        
+        glDrawArrays(GLenum(GL_TRIANGLES), 0, 3)
     }
 
 
