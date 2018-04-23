e1c2cc9aef3bec4eb9c489cb47734dc91a6e1867
stevewreese <stephenw.reese@gmail.com>
2018-04-23 17:17:40 -0600

    FRAGMENT SHADER

--- openGLtest/openGLtest/ViewController.swift
+++ openGLtest/openGLtest/ViewController.swift
@@ -17,13 +17,31 @@ class ViewController: GLKViewController {
         glkView.context = EAGLContext(api: .openGLES2)!
         EAGLContext.setCurrent(glkView.context)
         
-        let vertexShaderSource: String = """
+        let vertexShaderSource: NSString = """
         "
             void main(){
                 gl_Position = vec4(1.0, 1.0, 0.0, 1.0);
             }
         "
         """
+        
+        var vertexShaderSourceMemory = vertexShaderSource.utf8String
+        let vertexShader: GLuint = glCreateShader(GLenum(GL_VERTEX_SHADER))
+        glShaderSource(vertexShader, 1, &vertexShaderSourceMemory, nil)
+        glCompileShader(vertexShader)
+        
+        let fragmentShaderSource: NSString = """
+        "
+            void main(){
+                gl_Position = vec4(1.0, 1.0, 0.0, 1.0);
+            }
+        "
+        """
+        
+        var fragmentShaderSourceMemory = fragmentShaderSource.utf8String
+        let fragmentShader: GLuint = glCreateShader(GLenum(GL_FRAGMENT_SHADER))
+        glShaderSource(fragmentShader, 1, &fragmentShaderSourceMemory, nil)
+        glCompileShader(fragmentShader)
         
         glColor4f(1.0, 0.0, 0.0, 1.0)
         
