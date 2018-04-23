c39566e7bf3e497daff6be52eb63a917e8a5199c
stevewreese <stephenw.reese@gmail.com>
2018-04-23 15:18:35 -0600

    step 2

--- openGLtest/openGLtest/ViewController.swift
+++ openGLtest/openGLtest/ViewController.swift
@@ -6,13 +6,18 @@
 //  Copyright © 2018 u0396206. All rights reserved.
 //
 
-import UIKit
+import GLKit
 
-class ViewController: UIViewController {
+class ViewController: GLKViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let glkView: GLKView = view as! GLKView
+        glkView.context = EAGLContext(api: .openGLES2)!
+        EAGLContext.setCurrent(glkView.context)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
